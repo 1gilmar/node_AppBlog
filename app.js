9a7f04e7a86e4708acae1c9a7d02c85f9850e46a
jiraspiom <jiraspiom@yahoo.com.br>
2020-01-24 10:57:31 -0300

    Criado e configurado as cessoes e middleware foi add 2 variavel global

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 //npm install --save express-handlebars
 //npm install --save body-parser
 //npm install --save mongoose
+//npm install --save express-session
+//npm install --save connect-flash
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
@@ -9,8 +11,30 @@ const mongoose = require('mongoose')
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
+const session = require('express-session')
+const flash = require('connect-flash');
 
+//"function()" e mesma coisa que "() =>" ou seja, aerofunction.
 //configuracao
+    //sessao 
+    app.use(session({
+        secret: "",
+        resave: false,
+        saveUninitialized: true
+    }))
+    
+    //sempre deve ser depois da session
+    app.use(flash())
+    
+    //Configurando o middleware
+    app.use((req, res, next) => {
+        //criando variavel global
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+
+    })
+
     //bodyParse
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json());
