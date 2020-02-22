277f4319c6c64f626d91608adacff93f9465f744
jiraspiom <jiraspiom@yahoo.com.br>
2020-02-22 17:17:25 -0300

    add

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {montoURL: "link do bando de dados online" }
+    module.exports = {montoURL: "mongodb+srv://root:jiraspiom@cluster0-2gq4v.gcp.mongodb.net/test?retryWrites=true&w=majority" }
 }else{
     module.exports = {mongoURL: "mongodb://localhost:27017/blogapp"}
 }
